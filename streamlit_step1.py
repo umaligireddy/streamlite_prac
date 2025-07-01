d76a39855b00ac1b7349e2deba011dd7bca4513f
Umesh Reddy maligireddy <maligireddyumesh@gmail.com>
2025-07-01 11:58:57 -0400

    step2

--- streamlit_step1.py
+++ streamlit_step1.py
@@ -1,7 +1,10 @@
 import streamlit as st
-st.title("this is my first step")
-st.header("welcome to streamlit first step")
-st.subheader("hello umesh reddy")
-name=st.text_input("Enter your name")
-if st.button("greet me"):
-    st.success(f"hello! {name}, welcome here.")
+st.title("layout with columns")
+col1,col2= st.columns(2)
+with col1:
+    st.header("column 1")
+    st.subheader("this is column 1")
+with col2:
+    st.header("column2")
+    st.subheader("this is column 2")
+    

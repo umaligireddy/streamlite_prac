4d034485af6ca0f904a71787c516e667248af90c
Umesh Reddy maligireddy <maligireddyumesh@gmail.com>
2025-07-01 12:09:58 -0400

    step2.2

--- streamlit_step1.py
+++ streamlit_step1.py
@@ -1,10 +1,6 @@
 import streamlit as st
-st.title("layout with columns")
-col1,col2= st.columns(2)
-with col1:
-    st.header("column 1")
-    st.subheader("this is column 1")
-with col2:
-    st.header("column2")
-    st.subheader("this is column 2")
-    
+st.title("using sidebar")
+name = st.sidebar.text_input("enter your name")
+age = st.sidebar.number_input("enter your age", min_value=1, max_value=100)
+if st.sidebar.button("submit"):
+    st.success(f"hello,{name}! you are {age} years old.")

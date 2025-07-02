f91ba0a55f12e12ebc0df0741db519aba6a89b14
Umesh Reddy maligireddy <maligireddyumesh@gmail.com>
2025-07-02 19:26:00 -0400

    commit

--- file_uploder.py
+++ file_uploder.py
@@ -7,3 +7,16 @@ if uploaded_file is not None:
     st.success("file uploaded successfully")
     st.subheader("data preview")
     st.dataframe(df)
+    numeric_columns = df.select_dtypes(include=["float64", "int64"]).columns.tolist()
+    if numeric_columns:
+        selected_column = st.selectbox("select a numeric column to plot", numeric_columns)
+        chart_type = st.radio("select chart type",["bar chart","line chart"])
+        st.subheader("chart")
+        if chart_type =="bar cart":
+            st.bar_chart(df[selected_column])
+        else:
+            st.line_chart(df[selected_column])
+    else:
+        st.warning("no numeric column is selected to plot")
+else:
+    st.warning("please upload a cvs file")

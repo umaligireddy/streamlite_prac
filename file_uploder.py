9853559359149be72c32588fbe7c3982e2fa3dea
Umesh Reddy maligireddy <maligireddyumesh@gmail.com>
2025-07-02 19:56:57 -0400

    commit

--- file_uploder.py
+++ file_uploder.py
@@ -12,18 +12,18 @@ if uploaded_file is not None:
         selected_column = st.selectbox("select a numeric column to plot", numeric_columns)
         chart_type = st.radio("select chart type",["bar chart","line chart"])
         st.subheader("chart")
-        if chart_type =="bar cart":
+        if chart_type =="bar chart":
             st.bar_chart(df[selected_column])
         else:
             st.line_chart(df[selected_column])
     else:
         st.warning("no numeric column is selected to plot")
+    csv=df[[selected_column]].to_csv(index=False).encode("utf-8")
+    st.downlosd_button(
+            label="download selected column as csv",
+            data=csv,
+            file_name=f"{selected_column}.csv",
+            mime="text/csv"
+        )
 else:
     st.warning("please upload a cvs file")
-csv=df[selected_column].to_csv(index=False).encode("utf-8")
-st.downlosd_button(
-    lable="download selected column as csv",
-    data=csv,
-    file_name=f"{selected_column}.csv",
-    mime="text/csv"
-    )

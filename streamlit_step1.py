1976de19fe7be049da43b104abb2fdbfb4e3d90e
Umesh Reddy maligireddy <maligireddyumesh@gmail.com>
2025-07-02 17:22:44 -0400

    initial commit

--- streamlit_step1.py
+++ streamlit_step1.py
@@ -1,6 +1,5 @@
 import streamlit as st
-st.title("using sidebar")
-name = st.sidebar.text_input("enter your name")
-age = st.sidebar.number_input("enter your age", min_value=1, max_value=100)
-if st.sidebar.button("submit"):
-    st.success(f"hello,{name}! you are {age} years old.")
+st.title("Expander")
+with st.expander("click to see more"):
+    st.write("here you can see the hidden content inside the expander")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=150)
